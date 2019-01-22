583d3363c59c151357468f2ab947ae3c9b68b799
unitydemo2 <unitydemo2@yopmail.com>
2019-01-22 17:32:53 +0530

    DW CMS Message

--- ScriptRef/Log.cs
+++ ScriptRef/Log.cs
@@ -3,9 +3,19 @@ using System.Diagnostics;
 namespace DiffPlex
 {
     
+    /// <summary>
+    /// [5c46c35d04915438846c9457](CSharp Code_5c46c35d04915438846c9457.cs)
+    /// </summary>
+    /// <description>
+    /// [5c46c35d04915438846c9457](CSharp Code_5c46c35d04915438846c9457.cs)
+    /// </description>
     public static class Log
     {
         
+        /// <summary>
+        /// </summary>
+        /// <description>
+        /// </description>
         [Conditional("LOG")]
         public static void WriteLine(string format, params object[] args)
         {
@@ -13,6 +23,10 @@ namespace DiffPlex
         }
 
         
+        /// <summary>
+        /// </summary>
+        /// <description>
+        /// </description>
         [Conditional("LOG")]
         public static void Write(string format, params object[] args)
         {
