82c3d3defa745088c84f39aff86d5e61804b71f5
unitydemo2 <unitydemo2@yopmail.com>
2019-01-29 13:36:20 +0530

    DW CMS Message

--- ScriptRef/Model/DiffBlock.cs
+++ ScriptRef/Model/DiffBlock.cs
@@ -1,32 +1,47 @@
 namespace DiffPlex.Model
 {
-    ///     <summary>
-    ///     A block of consecutive edits from A and/or B
-    ///     </summary>
-        public class DiffBlock : DiffResult
+    /// <summary>
+            /// A block of consecutive edits from A and/or B
+            /// </summary>
+            /// <description>
+            /// </description>
+                    public class DiffBlock : DiffResult
     {
-        ///     <summary>
-        ///     Position where deletions in A begin
-        ///     </summary>
-                public int DeleteStartA { get; }
+        /// <summary>
+                        /// Position where deletions in A begin
+                        /// </summary>
+                        /// <description>
+                        /// </description>
+                                        public int DeleteStartA { get; }
 
-        ///     <summary>
-        ///     The number of deletions in A
-        ///     </summary>
-                public int DeleteCountA { get; }
+        /// <summary>
+                        /// The number of deletions in A
+                        /// </summary>
+                        /// <description>
+                        /// </description>
+                                        public int DeleteCountA { get; }
 
-        ///     <summary>
-        ///     Position where insertion in B begin
-        ///     </summary>
-                public int InsertStartB { get; }
+        /// <summary>
+                        /// Position where insertion in B begin
+                        /// </summary>
+                        /// <description>
+                        /// </description>
+                                        public int InsertStartB { get; }
 
-        ///     <summary>
-        ///     The number of insertions in B
-        ///     </summary>
-                public int InsertCountB { get; }
+        /// <summary>
+                        /// The number of insertions in B
+                        /// </summary>
+                        /// <description>
+                        /// </description>
+                                        public int InsertCountB { get; }
 
 
         
+        /// <summary>
+        /// [5c4054828e5c551c7085f5dd](Addedtaggroupto project_5c4054828e5c551c7085f5dd.cs)
+        /// </summary>
+        /// <description>
+        /// </description>
         public DiffBlock(int deleteStartA, int deleteCountA, int insertStartB, int insertCountB)
         {
             DeleteStartA = deleteStartA;
---
+++ xcv/Examples/Addedtaggroupto project_5c4054828e5c551c7085f5dd.cs
@@ -0,0 +1,55 @@
+{
+   "content":    {
+      "projectId": "5a79b23d3bfd782ab06d7fe9",
+      "tagGroups": ["5a79b22f3bfd782ab06d7fe7"]
+   },
+   "userId": "S-1-5-21-1210654208-2246142303-2829877410-29123",
+   "flowMap":    {
+      "cmsOperation": 30,
+      "eventGroups": [[      {
+         "eventName": 1040,
+         "eventLocationInFlowMap": null,
+         "status": 2,
+         "childCMSOperation": 0,
+         "childOperationStatus": null,
+         "eventCondition": null,
+         "reuseCmsOperation": 0,
+         "conditionKeys": null,
+         "requestMapProperty":          [
+                        {
+               "source": "ProjectId",
+               "target": "ProjectId"
+            },
+                        {
+               "source": "TagGroups",
+               "target": "TagGroups"
+            }
+         ],
+         "responseMapProperty":          [
+                        {
+               "source": "ProjectId",
+               "target": "ProjectId"
+            },
+                        {
+               "source": "TagGroups",
+               "target": "TagGroups"
+            }
+         ]
+      }]],
+      "notificationType": 2,
+      "notificationTopic": "Project",
+      "parentResponseId": null,
+      "parentEventLocation": null,
+      "isOperationCompleteWithStatusSuccess": true,
+      "_id": "5a72a5e44af10f8978deb4d3",
+      "status": 0,
+      "modifiedDate": 0,
+      "createdDate": 1517463012
+   },
+   "createdOn": "2018-02-06T13:48:49.88Z",
+   "errorResponse": null,
+   "_id": "5a79b24194bbfa38fc997807",
+   "status": 2,
+   "modifiedDate": 1517924930,
+   "createdDate": 1517924929
+}
