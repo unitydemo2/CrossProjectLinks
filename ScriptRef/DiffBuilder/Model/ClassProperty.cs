219247914d983874585cffb748db3ed9c08c3f71
CMSQATeam <cmsqa123@gmail.com>
2019-02-19 13:29:04 +0530

    DW CMS Message

--- ScriptRef/DiffBuilder/Model/ClassProperty.cs
+++ ScriptRef/DiffBuilder/Model/ClassProperty.cs
@@ -1,14 +1,20 @@
 namespace DiffPlex.DiffBuilder.Model
 {
     
+    /// <summary>
+    /// Testing 123
+    /// </summary>
     public class ClassProperty
     {
+        
         
         public int NumberA { get; set; }
 
         
+        
         public int NumberB { get; set; }
 
+        
         
         public int NumberC { get; set; }
     }
